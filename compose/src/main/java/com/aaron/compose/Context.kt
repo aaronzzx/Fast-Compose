e9778caff42a235bdcdebb4fd83cc9b414ea16d8
aaron <aaronzzxup@gmail.com>
2022-08-03 09:49:45 +0800

    增加 Context 扩展

---
+++ compose/src/main/java/com/aaron/compose/Context.kt
@@ -0,0 +1,28 @@
+package com.aaron.compose
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+/**
+ * 通过 Context 找到 Activity
+ */
+inline fun <reified T : Activity> Context.requireActivity(): T {
+    val activity = findActivity<T>()
+    require(activity != null) {
+        "Activity must not be null."
+    }
+    return activity
+}
+
+/**
+ * 通过 Context 找到 Activity
+ */
+inline fun <reified T : Activity> Context.findActivity(): T? {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is T) return context
+        context = context.baseContext
+    }
+    return null
+}
